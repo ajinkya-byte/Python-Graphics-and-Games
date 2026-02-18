c0d3fe6ddeb7cac1ae534831fe32053680e4f2d1
Ajinkya Jagtap <ajinkyajagtap28.07@gmail.com>
2026-02-18 13:47:22 +0530

    Implement advanced flower animation using turtle
    Added a turtle graphics flower animation with multiple colors.

--- Flower-Designs/advanced_flower_animation.py
+++ Flower-Designs/advanced_flower_animation.py
@@ -1,1 +1,19 @@
+import turtle
+
+screen = turtle.Screen()
+screen.bgcolor("black")
+
+flower = turtle.Turtle()
+flower.speed(0)
+flower.width(2)
+
+colors = ["red", "yellow", "pink", "orange", "purple", "cyan"]
+
+for i in range(60):
+    flower.color(colors[i % 6])
+    flower.circle(100)
+    flower.left(6)
+
+flower.hideturtle()
+turtle.done()
 
